d4a84749899a372c80f14a4a1e596440659f458b
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2025-04-27 14:41:46 -0400

    Testar POST de endpoint de OfertaViagem com autenticação JWT

--- JornadaMilhas.Integration.Test.API/JornadaMilhasWebApplicationFactory.cs
+++ JornadaMilhas.Integration.Test.API/JornadaMilhasWebApplicationFactory.cs
@@ -1,28 +1,49 @@
+using JornadaMilhas.API.DTO.Auth;
 using JornadaMilhas.Dados;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
-namespace JornadaMilhas.Integration.Test.API
+namespace JornadaMilhas.Integration.Test.API;
+
+public class JornadaMilhasWebApplicationFactory:WebApplicationFactory<Program>
 {
-    public class JornadaMilhasWebApplicationFactory:WebApplicationFactory<Program>
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        builder.ConfigureServices(services =>
         {
-            builder.ConfigureServices(services =>
-            {
-                services.RemoveAll(typeof(
-                    DbContextOptions<JornadaMilhasContext>));
-                services.AddDbContext<JornadaMilhasContext>(options =>
-                options
-                .UseLazyLoadingProxies()
-                .UseSqlServer
-                ("Server=localhost,11433;Database=JornadaMilhasV3;User Id=sa;Password=Alura#2024;Encrypt=false;TrustServerCertificate=true;MultipleActiveResultSets=true;"));
+            services.RemoveAll(typeof(
+                DbContextOptions<JornadaMilhasContext>));
+            services.AddDbContext<JornadaMilhasContext>(options =>
+            options
+            .UseLazyLoadingProxies()
+            .UseSqlServer
+            ("Server=localhost,11433;Database=JornadaMilhasV3;User Id=sa;Password=Alura#2024;Encrypt=false;TrustServerCertificate=true;MultipleActiveResultSets=true;"));
 
-            });
-            base.ConfigureWebHost(builder);
-        }
+        });
+        base.ConfigureWebHost(builder);
+    }
+    public async Task<HttpClient> GetClientWithAccessTokenAsync()
+    {
+        var client = this.CreateClient();
+
+        var user = new UserDTO
+        {
+            Email = "tester@email.com",
+            Password = "Senha123@"
+        };
+
+        var response = await client.PostAsJsonAsync("/auth-login", user);
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<UserTokenDTO>();
+
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result!.Token);
+
+        return client;
     }
 }
---
+++ JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
@@ -0,0 +1,38 @@
+﻿using JornadaMilhas.Dominio.Entidades;
+using JornadaMilhas.Dominio.ValueObjects;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Integration.Test.API;
+
+public class OfertaViagem_POST
+{
+    [Fact]
+    public async Task Cadastra_OfertaViagem()
+    {
+        //Arrange
+        var app = new JornadaMilhasWebApplicationFactory();
+
+        using var client = await app.GetClientWithAccessTokenAsync();
+
+        var OfertaViagem = new OfertaViagem()
+        {
+            Preco = 100,
+            Rota = new Rota("Origem", "Destino"),
+            Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+        };
+
+
+        //Act
+        var response = await client.PostAsJsonAsync("/ofertas-viagem", OfertaViagem);
+        //Assert
+        Assert.NotNull(response);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
